5df2be480434c96d9d88c2d6e0d1c6ef9ff2b26a
heraldofortuna <heraldofortuna@gmail.com>
2021-03-29 10:42:09 -0500

    I fixed the app name in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-app-heroku"));
+app.use(express.static("./dist/pruebaAngular"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/pruebaAngular/src/" })
+  res.sendFile("index.html", { root: "dist/pruebaAngular/" })
 );
 
 // Start the app by listening on the default Heroku port
